cf9c9365c92aeaec709970c8435d031ee5502c53
unknown <lixifan@qq.com>
2024-08-05 00:36:37 +0800

    feat:增加云函数测试代码

---
+++ cloudfunctions/addDevice/index.js
@@ -0,0 +1,15 @@
+// 云函数入口文件
+const cloud = require("wx-server-sdk");
+
+cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }); // 使用当前云环境
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const db = cloud.database()
+  const todos = db.collection("device_list");
+  const list = await todos.where({}).get();
+  console.log("list", list);
+  return {
+    test: list,
+  };
+};
